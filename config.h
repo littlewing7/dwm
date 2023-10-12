9d0661b46edef0a5091038a4d5886c672883877f
Massimo Manzato <massimo.manzato@gmail.com>
2023-10-12 16:54:56 +0200

    reduced dmenu fonts

--- config.h
+++ config.h
@@ -10,7 +10,7 @@ static const int topbar             = 1;        /* 0 means bottom bar */
  * */
 /*static const char dmenufont[]       = "monospace:size=10";*/
 static const char *fonts[]          = { "DejaVu Sans:size=10" };
-static const char dmenufont[]       = "DejaVu Sans:size=10";
+static const char dmenufont[]       = "DejaVu Sans:size=8";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -32,7 +32,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "blueman-manager",    NULL,       NULL,       1 << 3,       0,           -1 },
+	{ "Blueman-Manager",    NULL,       NULL,       1 << 3,       0,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "Mixxx",    NULL,       NULL,       1 << 4,       0,           -1 },
 };
