7cca4931bdbfc3bef8464066a1998bdd5cee6e1a
Massimo Manzato <massimo.manzato@gmail.com>
2023-10-12 17:12:19 +0200

    fix Bluetooth Devices tag 4

--- config.h
+++ config.h
@@ -32,7 +32,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Bluetooth-Devices",    NULL,       NULL,       1 << 3,       0,           -1 },
+	{ "Bluetooth Devices",    NULL,       NULL,       1 << 3,       0,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "Mixxx",    NULL,       NULL,       1 << 4,       0,           -1 },
 };
